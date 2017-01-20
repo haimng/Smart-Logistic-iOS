dea05aef775041cbfef067c63da7dc5dc4b1d95d
Hai M Nguyen <vnjp22@gmail.com>
2017-01-20 12:23:35 -0800

    Init project: Menu, SearchBar, Sign Up/In, Profile, Networking

---
+++ Package/Sources/Utils/UIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController.swift
+//  SmartLogistic
+//
+//  Created by Hai Minh Nguyen on 7/18/16.
+//  Copyright © 2016 VNDB Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func setNavigationBarItem() {
+        self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
+//        self.addRightBarButtonWithImage(UIImage(named: "ic_notifications_black_24dp")!)
+        self.slideMenuController()?.removeLeftGestures()
+        self.slideMenuController()?.removeRightGestures()
+        self.slideMenuController()?.addLeftGestures()
+//        self.slideMenuController()?.addRightGestures()
+    }
+    
+    func removeNavigationBarItem() {
+        self.navigationItem.leftBarButtonItem = nil
+        self.navigationItem.rightBarButtonItem = nil
+        self.slideMenuController()?.removeLeftGestures()
+        self.slideMenuController()?.removeRightGestures()
+    }
+    
+}
